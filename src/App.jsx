3936be06b16ea3d4ccbb403f1331711c453fc733
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-05-18 23:07:52 -0400

    Crea Navbar, Home, Pokemones

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './views/Home';
+import Pokemones from './views/Pokemones';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:name" element={<Pokemones />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/views/Pokemones.jsx
@@ -0,0 +1,3 @@
+export default function Pokemones() {
+  return <div>Pokemones</div>;
+}
