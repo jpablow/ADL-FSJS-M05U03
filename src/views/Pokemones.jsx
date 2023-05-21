00fd6c9f80d47845f65dc89f77ee8a20f919b042
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-05-20 23:00:53 -0400

    Crea options para select de view Pokemones desde API

--- src/views/Pokemones.jsx
+++ src/views/Pokemones.jsx
@@ -1,16 +1,43 @@
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
 
 export default function Pokemones() {
+  const [options, setOptions] = useState([]);
+
+  const getOptions = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon';
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const results = data.results.map((d) => d.name);
+    setOptions(results);
+  };
+
+  useEffect(() => {
+    return () => {
+      getOptions();
+    };
+  }, []);
+
   return (
     <Container className="text-center">
       <h1 className="mt-4">Selecciona un pokemón</h1>
 
-      <Form.Select aria-label="Default select example" className="mt-4">
-        <option disabled>Selecciona un pokemón</option>
-        {/* insertar map para options */}
-        <option value="1">One</option>
+      <Form.Select
+        aria-label="Default select example"
+        className="mt-4"
+        defaultValue={'x'}
+        onChange={(e) => console.log(e.target.value)}
+      >
+        <option disabled value={'x'}>
+          Selecciona un pokemón
+        </option>
+        {options.map((op, index) => (
+          <option key={index} value={op}>
+            {op}
+          </option>
+        ))}
       </Form.Select>
 
       <Button className="btn-dark mt-4">Ver detalle</Button>
